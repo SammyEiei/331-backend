2cc8e85775703f6a53b6c0bf3c565a708e8421b5
Wasan Renumat <wasanrenumat@Wasans-MacBook-Pro-2.local>
2024-09-27 00:55:59 +0700

    Q 3.6

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 import se331.lab.rest.service.EventService;
+import se331.lab.rest.util.LabMapper;
 
 
 @RequiredArgsConstructor
@@ -40,7 +41,7 @@ public ResponseEntity<?> getEventLists(
     Page<Event> pageOutput = eventService.getEvents(perPage, page);
     HttpHeaders responseHeader = new HttpHeaders();
     responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
-    return new ResponseEntity<>(pageOutput.getContent(), responseHeader, HttpStatus.OK);
+    return new ResponseEntity<>(LabMapper.INSTANCE.getEventDto(pageOutput.getContent()), responseHeader, HttpStatus.OK);
 }
 
 
@@ -48,7 +49,7 @@ public ResponseEntity<?> getEventLists(
     public ResponseEntity<?> getEventById(@PathVariable("id") Long id)  {
         Event output = eventService.getEvent(id);
         if(output != null) {
-            return ResponseEntity.ok(output);
+            return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
         }
         else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The given id is not found");
@@ -57,7 +58,7 @@ public ResponseEntity<?> getEventLists(
     @PostMapping("/events")
     public ResponseEntity<?> addEvent(@RequestBody Event event) {
         Event output = eventService.save(event);
-        return ResponseEntity.ok(output);
+        return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
     }
 
 
